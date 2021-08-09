6362e5f0c64067534ca6fc0bb88101d9ece275f1
Tarun Kumar <tarun.kumar@softobiz.com>
2021-08-09 13:11:27 +0530

    Text Echo test added

--- ArrayCheckingTest.php
+++ ArrayCheckingTest.php
@@ -10,7 +10,7 @@ $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
 $a2=array("a"=>"purple","b"=>"orange");
 array_splice($a1,0,2,$a2);
 print_r($a1);
-
+echo "Test";
 $z = ['me','you', 'he'];
 array_unshift($z, 'she', 'it');
 print_r($z);
